e133e7ed0a9df542db815a00b56b609aca493b2c
Sven Luijten <svenluijten@outlook.com>
2016-03-23 17:37:46 +0100

    Add test

--- tests/ViewCreatorTest.php
+++ tests/ViewCreatorTest.php
@@ -60,10 +60,29 @@ class ViewCreatorTest extends BaseTest
     public function it_creates_multiple_sections()
     {
         $this->view->create('index')->sections(['content', 'scripts']);
+        $this->view->create('about')->section('content')->section('scripts');
 
         $this->assertEquals(
             '@section(\'content\')' . PHP_EOL . PHP_EOL . '@endsection' . PHP_EOL .
             '@section(\'scripts\')' . PHP_EOL . PHP_EOL . '@endsection' . PHP_EOL,
+            file_get_contents(__DIR__ . '/assets/index.blade.php')
+        );
+
+        $this->assertEquals(
+            '@section(\'content\')' . PHP_EOL . PHP_EOL . '@endsection' . PHP_EOL .
+            '@section(\'scripts\')' . PHP_EOL . PHP_EOL . '@endsection' . PHP_EOL,
+            file_get_contents(__DIR__ . '/assets/about.blade.php')
+        );
+    }
+
+    /** @test */
+    public function it_extends_a_view_and_has_a_section()
+    {
+        $this->view->create('index')->extends('app')->section('content');
+
+        $this->assertEquals(
+            '@extends(\'app\')' . PHP_EOL .
+            '@section(\'content\')' . PHP_EOL . PHP_EOL . '@endsection' . PHP_EOL,
             file_get_contents(__DIR__ . '/assets/index.blade.php')
         );
     }
