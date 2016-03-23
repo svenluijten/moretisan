5ff9bd7b71abe7cd3242747f43715999fab1a5c2
Sven Luijten <svenluijten@outlook.com>
2016-03-23 18:54:34 +0100

    Finishing touches

--- src/Commands/MakeViewCommand.php
+++ src/Commands/MakeViewCommand.php
@@ -3,8 +3,58 @@
 namespace Sven\Moretisan\Commands;
 
 use Illuminate\Console\Command;
+use Sven\Moretisan\MakeView\ViewCreator;
 
 class MakeViewCommand extends Command
 {
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:view
+                           {name : The name of the view to create}
+                           {--extends= : What \'master\' view should be extended?}
+                           {--sections= : A comma-separated list of sections to create.}
+                           {--directory=resources/views/ : The directory where your views are stored.}
+                           {--extension=blade.php : What file extension should the view have?}';
 
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new view';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $view = new ViewCreator(
+            base_path($this->option('directory'))
+        );
+
+        $view = $view->create(
+            $this->argument('name'),
+            $this->option('extension')
+        );
+
+        if ( ! is_null( $this->argument('extends') )) {
+            $view = $view->extends($this->argument('extends'));
+        }
+
+        if ( ! is_null( $this->argument('sections') )) {
+            $view = $view->sections(
+                explode(',', $this->argument('sections'));
+            );
+        }
+
+        return $this->info(printf(
+            'Successfully created the view \'%s\'!',
+            $this->argument('name')
+        ));
+    }
 }
--- src/MoretisanServiceProvider.php
+++ src/MoretisanServiceProvider.php
@@ -2,7 +2,33 @@
 
 namespace Sven\Moretisan;
 
+use Illuminate\Support\ServiceProvider;
+
 class MoretisanServiceProvider extends ServiceProvider
 {
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->app['view:make'] = $this->app->share(function () {
+            return new Commands\MakeViewCommand();
+        });
 
+        $this->commands(
+            'make:view'
+        );
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
 }
