c9f45aaa6e03b42aba756505baf6bfb1b4924cfe
Sven Luijten <svenluijten@outlook.com>
2016-03-31 21:03:33 +0200

    Refactor logic into separate class

--- src/Components/MakeView/MakeView.php
+++ src/Components/MakeView/MakeView.php
@@ -3,43 +3,16 @@
 namespace Sven\Moretisan\Components\MakeView;
 
 use Illuminate\Support\Str;
-use Sven\Moretisan\Exceptions\FileAlreadyExists;
+use Sven\Moretisan\Shared\FileInteractor;
 
-class MakeView
+class MakeView extends FileInteractor
 {
-    /**
-     * Full path where the file to create should be located.
-     *
-     * @var string
-     */
-    protected $path;
-
-    /**
-     * Base path where your views are located.
-     *
-     * @var string
-     */
-    protected $base;
-
     /**
      * Full path to the file.
      *
      * @var string
      */
     protected $file;
-
-    /**
-     * Instantiate the MakeView component.
-     *
-     * @param string $path Path to create the views in.
-     */
-    public function __construct($path)
-    {
-        $realPath = realpath($path);
-
-        $this->path = $realPath;
-        $this->base = $realPath;
-    }
 
     /**
      * Create a new view file.
@@ -83,19 +56,6 @@ class MakeView
     }
 
     /**
-     * Set paths back to their defaults.
-     *
-     * @return \Sven\Moretisan\Components\MakeView\MakeView
-     */
-    public function clean()
-    {
-        $this->path = $this->base;
-        $this->file = '';
-
-        return $this;
-    }
-
-    /**
      * Extend a layout file.
      *
      * @param  string $name The name of the file to extend.
@@ -128,37 +88,6 @@ class MakeView
     }
 
     /**
-     * Append to the current file.
-     *
-     * @param  string $content Content to append.
-     * @return void
-     */
-    protected function appendToFile($content)
-    {
-        file_put_contents($this->file, $content, FILE_APPEND);
-    }
-
-    /**
-     * Normalize a string to an array.
-     *
-     * @param  string|array $value     The value to normalize.
-     * @param  string       $delimiter Delimiter to explode by.
-     * @return array                   Normalized array of values.
-     */
-    protected function normalizeToArray($value, $delimiter)
-    {
-        if (is_array($value)) {
-            return $value;
-        }
-
-        if ( ! Str::contains($value, $delimiter)) {
-            return [$value];
-        }
-
-        return explode($delimiter, $value);
-    }
-
-    /**
      * Get a stub by name and replace optional parameters.
      *
      * @param  string $name   Name of the stub.
@@ -174,69 +103,5 @@ class MakeView
         }
 
         return $stub;
-    }
-
-    /**
-     * Recursively create folders.
-     *
-     * @param  array  $folders Folders to create inside each other.
-     * @return void
-     */
-    protected function createFolders($folders)
-    {
-        if (empty($folders)) {
-            return;
-        }
-
-        $path = $this->addToPath(array_pop($folders));
-
-        if ( ! is_dir($path)) {
-            mkdir($path);
-        }
-
-        return $this->createFolders($folders);
-    }
-
-    /**
-     * Add given folder to the path property.
-     *
-     * @param string $folder The folder to add to the path.
-     */
-    protected function addToPath($folder)
-    {
-        $this->path .= "/$folder";
-
-        return $this->path;
-    }
-
-    /**
-     * Create a file from the filename and extension.
-     *
-     * @param  string $filename  The name of the file.
-     * @param  string $extension The extension of the file.
-     * @return void
-     */
-    protected function makeFile($filename, $extension)
-    {
-        $extension = $this->parseExtension($extension);
-
-        $this->file = $this->path.'/'.$filename.$extension;
-
-        if (file_exists($this->file)) {
-            throw new FileAlreadyExists("The file at [$this->file] already exists.");
-        }
-
-        file_put_contents($this->file, '');
-    }
-
-    /**
-     * Normalize the extension so it starts with a period.
-     *
-     * @param  string $extension The extension to normalize.
-     * @return string
-     */
-    protected function parseExtension($extension)
-    {
-        return Str::startsWith($extension, '.') ? $extension : ".$extension";
     }
 }
